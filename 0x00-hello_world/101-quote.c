cffa952f2eb566acb72bbc34867718a3f3e8cbc3
Tanya-abi <tchigwda6@gmail.com>
2022-09-08 16:22:17 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+* main - printing quote
+*
+* Description: utilise write 
+* Printing "and that piece of art is useful" - Dora Korpar, 2015-10-19 
+* Followed by a new line, to the standard error.
+* Return: Always 1 (Success)
+*/
+
+int main (void)
+{
+	write(2 "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}
+
