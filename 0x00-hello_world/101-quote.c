b7a65a1271d861c151972b66f3e9a09e71bd59eb
Tanya-abi <tchigwda6@gmail.com>
2022-09-08 16:36:51 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,7 +2,7 @@
 #include <unistd.h>
 
 /**
-* main - printing quote
+main - prints Dora Korpars's quote
 *
 * Description: utilise write 
 * Printing "and that piece of art is useful" - Dora Korpar, 2015-10-19 
@@ -12,7 +12,7 @@
 
 int main (void)
 {
-	write(2 "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
 	return (1);
 }
 
