3f33c9dcf8f5e47fd992f5a6c5b039164f5d8ae9
Tanya-abi <tchigwda6@gmail.com>
2022-09-12 12:18:42 -0700

    If else

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+* main - prints all possible combinations of single-digit numbers
+*
+* Description: use the putchar and main function
+* You can only use putchar four times maximum in your code
+* You are not allowed to use any variable of type char
+* Return: 0
+*/
+int main(void)
+{
+	int c;
+	for (c = 48; c <= 57; c++)
+	{
+		putchar(c);
+		if (c != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
