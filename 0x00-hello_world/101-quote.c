582d9d7cde68ed189c146c9828493552e03bcd62
Tanya-abi <tchigwda6@gmail.com>
2022-12-08 09:08:28 -0800

    Hello world

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <unistd
+
+/*
+ * main - prints exactly and that piece of art is useful
+ *
+ * Description: utilise write
+ * Printing "and that piece of art is useful" - Dora Korpar, 2015-10-19
+ * Followed by a new line, to the standard error.
+ * Return: Always 1 (Success)
+ */
+
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
