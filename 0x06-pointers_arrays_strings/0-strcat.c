ad833f8ec86204474a720c7858f4e30dcd2d16f6
Tanya-abi <tchigwda6@gmail.com>
2022-12-22 06:59:31 -0800

    PAS_2

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,16 +9,17 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int i;
-	int n = 0;
+	int i, c;
 
-	while (dest[n] != '\0')
+	for (i = 0; dest[i] != '\0'; i++)
+		;
+
+	for (c = 0; src[c] != '\0'; c++)
 	{
-		n++
+		dest[i] = src[c];
+		i++;
 	}
-	for (i = 0 ; i < n && src[i] != '\0' ; i++)
-		dest[n + i] = src[i];
-	dest[n + i] = '\0';
 
+	dest[i] = '\0';
 	return (dest);
 }
